3afd3bf32070b874ab624253af5c5498802eea87
SamandarYigitaliyev <programmist4467@gmail.com>
2023-09-15 23:28:21 +0500

    Footer Language added

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,22 +1,24 @@
-<template>    
-<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
-    </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline">Contact</a>
-        </li>
-    </ul>
-    </div>
-</footer>
+<script setup lang="ts">
+</script>
+<template>
+    <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 Green Sale
+            </span>
+            <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">{{ $t("About") }}</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">{{ $t("PrivacyPolicy") }}</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">{{ $t("TermsConditions") }}</a>
+                </li>
+                <li>
+                    <a href="https://t.me/GreenSale_Uz" class="hover:underline">{{ $t("Telegram") }}</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
 </template>
--- src/components/common/LanguageSwitcher.vue
+++ src/components/common/LanguageSwitcher.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Icon from '@/assets/icons/Icon.vue';
+import Icon from '../../assets/icons/Icon.vue';
 import {ref} from 'vue';
 import { useI18n } from 'vue-i18n'
 const t = useI18n()
