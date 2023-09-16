d047dd3842dc0d941dd4f2f85c11fe5a68f803d1
AbdurahmonJumanboyev <abdurahmonnt2003@gmail.com>
2023-09-16 19:39:47 +0500

    CategoryPage Implemented

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ import App from './App.vue'
 import './index.css'
 import router from './router'
 import i18n from './locales/i18n'
-import VueApexCharts from "vue3-apexcharts";
+//import VueApexCharts from "vue3-apexcharts";
 
 
 const app = createApp(App)
 
 
-app.use(VueApexCharts);
+//app.use(VueApexCharts);
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
