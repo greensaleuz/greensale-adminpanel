de4f1a4cf87b75d20551dc2b3ba90b9103cba54c
SamandarYigitaliyev <programmist4467@gmail.com>
2023-09-16 23:53:43 +0500

    IconFixed

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,14 @@ import App from './App.vue'
 import './index.css'
 import router from './router'
 import i18n from './locales/i18n'
-//import VueApexCharts from "vue3-apexcharts";
+import Icon from './assets/icons/Icon.vue'
+import VueApexCharts from "vue3-apexcharts";
 
 
 const app = createApp(App)
 
-
-//app.use(VueApexCharts);
+app.component('icon',Icon)
+app.use(VueApexCharts);
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
