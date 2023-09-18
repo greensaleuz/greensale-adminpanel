2121c44be2f91e57f10da66defcfbe78ab44a761
AbdurahmonJumanboyev <abdurahmonnt2003@gmail.com>
2023-09-18 09:28:04 +0500

    Admin Created

---
+++ src/viewmodels/AdminViewModels.ts
@@ -0,0 +1,12 @@
+export class AdminViewModel{
+    id: number = 0;
+    firstName: string = "";
+    lastName: string = "";
+    phoneNumber: string = "";
+    phoneNuberConfirme: boolean = false;
+    region: string = "";
+    district: string = "";
+    address: string = "";
+    createdAt: Date = new Date();
+    updatedAt: Date = new Date();
+}
