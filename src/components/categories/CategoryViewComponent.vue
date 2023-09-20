e708d7df592b2fffc513ce1f8ac5da6309087b97
AbdurahmonJumanboyev <abdurahmonnt2003@gmail.com>
2023-09-20 11:58:58 +0500

    DataHelper fixed

--- src/components/categories/CategoryViewComponent.vue
+++ src/components/categories/CategoryViewComponent.vue
@@ -25,6 +25,7 @@ export default defineComponent({
   },
   methods: {    
     getBaseURL(){
+      debugger;
       this.baseURL = axios.defaults.baseURL!;
       this.createdAtString = formatDate(this.createdAt!);
       this.updatedAtString = formatDate(this.createdAt!);
