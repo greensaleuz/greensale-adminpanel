d58253623dc5d3c7e26afac4352a98564ad6f24b
SamandarYigitaliyev <programmist4467@gmail.com>
2023-09-22 23:47:53 +0500

    UserSearch Errors Fixed

--- src/components/sellerposts/SellerCoroselItemComponent.vue
+++ src/components/sellerposts/SellerCoroselItemComponent.vue
@@ -1,43 +1,43 @@
 <script lang="ts">
 
- import axios from '../../plugins/axios'
-  import FlowbiteSetup from "../../FlowbiteSetup.vue"
-  import { defineComponent } from "vue";
-  
-  export default defineComponent({
-      components:{
-      FlowbiteSetup,
-  },
-      props: {
-        ImageList:[] 
-      },
+import axios from '../../plugins/axios'
+import FlowbiteSetup from "../../FlowbiteSetup.vue"
+import { defineComponent } from "vue";
 
-      data() {
-          return{
-            
-              imageFullPath: "" as String,
-          }
-      },
-      methods: {
-          load(){
-            debugger
-              this.baseURL = axios.defaults.baseURL!;
-              console.log(this.imagePath)
-              
-          },
-  
-      },
-      mounted() {
-          this.load();
-      }
-  });</script>
+export default defineComponent({
+    components: {
+        FlowbiteSetup,
+    },
+    props: {
+        ImageList: []
+    },
+
+    data() {
+        return {
+            baseURL:'' as string,
+            imageFullPath: "" as String,
+        }
+    },
+    methods: {
+        load() {
+            this.baseURL = axios.defaults.baseURL!;
+           // console.log(this.imagePath)
+
+        },
+
+    },
+    mounted() {
+        this.load();
+    }
+});
+</script>
 
 <template>
     <div id="gallery" class="relative w-full my-5" data-carousel="slide">
         <!-- Carousel wrapper -->
         <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
             <!-- Item 1 -->
-            <div v-for="element in imagePath" :key="element">
+            <div v-for="element in imageFullPath" :key="element">
 
                 <div className="hidden duration-700 ease-in-out" data-carousel-item>
                     <img :src=element.toString()
@@ -78,40 +78,51 @@
     </div>
 
 
-  <div id="gallery" class="relative w-full my-5" data-carousel="slide" >
-      <!-- Carousel wrapper -->
-      <div class="relative h-56 overflow-hidden rounded-lg md:h-96" >
-           <!-- Item 1 -->
-           <div v-for="element in ImageList" :key="element">
-          
-          <div className="hidden duration-700 ease-in-out" data-carousel-item >
-                  <img :src=element className="absolute block max-w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" />
-          </div>
-      </div>
-          
-      </div>
-  
-      <!-- Slider controls -->
-      <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-          <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-              <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-              </svg>
-              <span class="sr-only">Previous</span>
-          </span>
-      </button>
-      <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-          <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-              <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-              </svg>
-              <span class="sr-only">Next</span>
-          </span>
-      </button>
-  </div>
-  
-  
-  <FlowbiteSetup></FlowbiteSetup>
-  </template>
+    <div id="gallery" class="relative w-full my-5" data-carousel="slide">
+        <!-- Carousel wrapper -->
+        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+            <!-- Item 1 -->
+            <div v-for="element in ImageList" :key="element">
+
+                <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img :src=element.toString()
+                        className="absolute block max-w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                        alt="" />
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Slider controls -->
+        <button type="button"
+            class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            data-carousel-prev>
+            <span
+                class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M5 1 1 5l4 4" />
+                </svg>
+                <span class="sr-only">Previous</span>
+            </span>
+        </button>
+        <button type="button"
+            class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            data-carousel-next>
+            <span
+                class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 9 4-4-4-4" />
+            </svg>
+            <span class="sr-only">Next</span>
+        </span>
+    </button>
+</div>
+
+
+<FlowbiteSetup></FlowbiteSetup></template>
 
   
