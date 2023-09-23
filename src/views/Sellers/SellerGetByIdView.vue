7dbef6e0f6b81a546a30f93347214494c0b46aef
AbdurahmonJumanboyev <abdurahmonnt2003@gmail.com>
2023-09-23 05:04:08 +0500

    SellerDelete Fixed

--- src/views/Sellers/SellerGetByIdView.vue
+++ src/views/Sellers/SellerGetByIdView.vue
@@ -351,7 +351,6 @@ export default defineComponent({
               </button>
               <!--Two Star-->
               <!--Two Star-->
-
               <button @click="stars(5)" v-show="star_five == false">
                 <svg class="w-4 h-4 text-gray-200 dark:text-gray-600 mr-1" aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
